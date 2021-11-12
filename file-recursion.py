8b96f007d1501f9acda359b5e1731ef0c101a899
Kunal Dubey <kunaldubey2297@gmail.com>
2021-11-13 00:34:11 +0530

    completed task 2

---
+++ file-recursion.py
@@ -0,0 +1,25 @@
+import os 
+
+testpath = './testdir'
+# suffix is extension  
+
+def recursive_search(path,list):
+    list.append(path)
+    if(os.path.isdir(path)): 
+        dirlist = os.listdir(path) 
+        for dir in dirlist:  
+            realPath = os.path.realpath(path+'/'+dir) 
+            
+            recursive_search(realPath,list) 
+    return list  
+
+def find_files(suffix,path):
+    paths = [] 
+    realPath = os.path.realpath(path)
+    dirlist = recursive_search(realPath,paths) 
+    for path in dirlist:
+        if(".c" in path):
+            print(path)
+
+
+find_files('.c',testpath)
