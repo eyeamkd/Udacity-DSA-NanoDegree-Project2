7d8362218ec411540f8860e208c2df7ae81c04e8
Kunal Dubey <kunaldubey2297@gmail.com>
2021-11-30 04:53:29 +0530

    completed the huffman_coding stripping spaces part

--- huffman_coding.py
+++ huffman_coding.py
@@ -1,8 +1,20 @@
-import sys
+import sys 
+
+
+def determine_frequency(string:str): 
+    dictionary = {} 
+    string = string.replace(" ","") 
+    # print("Stripped string is ", string);
+    for letter in string:
+        if(letter in dictionary.keys()):
+            dictionary[letter]+=1 
+        else:
+            dictionary[letter]=1 
+    return dictionary
 
 def huffman_encoding(): 
 
-    pass
+    pass 
 
 def huffman_decoding():
     pass  
@@ -10,17 +22,19 @@ def huffman_decoding():
 if __name__ == "__main__":
     codes = {}
 
-    a_great_sentence = "The bird is the word"
+    a_great_sentence = "The bird is the word" 
+    result = determine_frequency(a_great_sentence) 
+    print(result)  
+    
+    # print ("The size of the data is: {}\n".format(sys.getsizeof(a_great_sentence)))
+    # print ("The content of the data is: {}\n".format(a_great_sentence))
 
-    print ("The size of the data is: {}\n".format(sys.getsizeof(a_great_sentence)))
-    print ("The content of the data is: {}\n".format(a_great_sentence))
+    # encoded_data, tree = huffman_encoding(a_great_sentence)
 
-    encoded_data, tree = huffman_encoding(a_great_sentence)
+    # print ("The size of the encoded data is: {}\n".format(sys.getsizeof(int(encoded_data, base=2))))
+    # print ("The content of the encoded data is: {}\n".format(encoded_data))
 
-    print ("The size of the encoded data is: {}\n".format(sys.getsizeof(int(encoded_data, base=2))))
-    print ("The content of the encoded data is: {}\n".format(encoded_data))
+    # decoded_data = huffman_decoding(encoded_data, tree)
 
-    decoded_data = huffman_decoding(encoded_data, tree)
-
-    print ("The size of the decoded data is: {}\n".format(sys.getsizeof(decoded_data)))
-    print ("The content of the encoded data is: {}\n".format(decoded_data))
+    # print ("The size of the decoded data is: {}\n".format(sys.getsizeof(decoded_data)))
+    # print ("The content of the encoded data is: {}\n".format(decoded_data))
