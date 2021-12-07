ef8557d6460aff35a96b3e4f827c0a16ed738bc3
Kunal Dubey <kunaldubey2297@gmail.com>
2021-12-08 01:09:35 +0530

    able to create the huffman tree

--- huffman_coding.py
+++ huffman_coding.py
@@ -1,5 +1,6 @@
 import sys 
 from queue import PriorityQueue 
+from dataclasses import dataclass, field
 from typing import Any
 
 class Huffman_Node: 
@@ -7,7 +8,7 @@ class Huffman_Node:
         self.data = data 
         pass  
     
-    def getCharacter(self):
+    def getData(self):
         return self.data  
     
     def insertLeft(self, node):
@@ -37,49 +38,12 @@ class Huffman_Node:
             new_node.insertRight(self) 
         return new_node
 
+
+@dataclass(order=True)
 class PrioritizedItem:
     priority: int
-    item : Any
-    def __init__(self, priority, item) -> None:
-        self.priority = priority 
-        self.item = item 
+    item: Any=field(compare=False)
         
-class Heap_node: 
-    
-    def __init__(self,value) -> None: 
-        self.value = value
-        self.leftNode = None 
-        self.rightNode = None  
-    
-    def insertLeft(self,node):
-        self.leftNode = node
-    
-    def insertRight(self,node):
-        self.rightNode = node 
-    
-    def getLeft(self):
-        return self.leftNode 
-    
-    def getRight(self):
-        return self.rightNode 
-
-class priority_queue:
-    
-    def __init__(self,value, order="ASC") -> None: 
-        self.root = Heap_node(value)
-        self.size = 0 
-        self.order = order
-    
-    def size(self):
-        return self.size 
-    
-    def insert(self,value):   
-        #CHALLENGE: IMAGINE A COMPLETE BINARY HEAP, 
-        # AND THEN SUDDENLY YOU GOT TO INTRODUCE A NODE 
-        # WHICH WOULD MEAN TRAVERSING THE TREE AND REPLACING THE ROOT
-        pass
-        
-
 
 def determine_frequency(string:str): 
     dictionary = {} 
@@ -105,34 +69,27 @@ def huffman_encoding(string :str):
     priority_queue = create_priority_queue(frequency_dictionary) 
     root_node = build_huffman_tree(priority_queue) 
     print("The root node is here", root_node)
-    # print("Lowest priority is", priority_queue.get()[1])
-    # print(priority_queue) 
 
 def lowest_frequency_nodes(queue): 
     return queue.get(), queue.get()
     pass 
 
-def merge(node1, node2):
-    total_frequency = node1[0] + node2[0] 
+def merge(node1:PrioritizedItem, node2:PrioritizedItem): 
+    total_frequency = node1.priority + node2.priority
     merged_node = Huffman_Node(total_frequency) 
-    merged_node.leftNode = Huffman_Node(node1[0]) 
-    merged_node.rightNode = Huffman_Node(node2[0]) 
+    merged_node.leftNode = Huffman_Node(node1.item) 
+    merged_node.rightNode = Huffman_Node(node2.item) 
     return merged_node 
     
     
 def build_huffman_tree(queue:PriorityQueue): 
-    while(queue.qsize()!=1):
+    while(queue.qsize()>1):
         node1, node2 = lowest_frequency_nodes(queue) 
         merged_node = merge(node1, node2)  
         print("Combined frequency of the merged nodes is", merged_node.data)
         queue.put(PrioritizedItem(merged_node.data,merged_node)) 
     
-    return queue
-         
-        
-    pass    
-
-
+    return queue.get().item
     
    
 
@@ -145,17 +102,3 @@ if __name__ == "__main__":
     a_great_sentence = "The bird is the word" 
     
     queue = huffman_encoding(a_great_sentence)
-    
-    
-    # print ("The size of the data is: {}\n".format(sys.getsizeof(a_great_sentence)))
-    # print ("The content of the data is: {}\n".format(a_great_sentence))
-
-    # encoded_data, tree = huffman_encoding(a_great_sentence)
-
-    # print ("The size of the encoded data is: {}\n".format(sys.getsizeof(int(encoded_data, base=2))))
-    # print ("The content of the encoded data is: {}\n".format(encoded_data))
-
-    # decoded_data = huffman_decoding(encoded_data, tree)
-
-    # print ("The size of the decoded data is: {}\n".format(sys.getsizeof(decoded_data)))
-    # print ("The content of the encoded data is: {}\n".format(decoded_data))
