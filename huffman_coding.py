a1bd8f4b5f9b4c49ea7eb38a802516d3ac74daca
Kunal Dubey <kunaldubey2297@gmail.com>
2021-12-08 01:26:01 +0530

    able to retieve individual data from the node'

--- huffman_coding.py
+++ huffman_coding.py
@@ -5,7 +5,9 @@ from typing import Any
 
 class Huffman_Node: 
     def __init__(self, data) -> None: 
-        self.data = data 
+        self.data = data  
+        self.leftNode = None
+        self.rightNode = None
         pass  
     
     def getData(self):
@@ -67,7 +69,8 @@ def create_priority_queue(frequency_dictionary:dict):
 def huffman_encoding(string :str): 
     frequency_dictionary = determine_frequency(string)   
     priority_queue = create_priority_queue(frequency_dictionary) 
-    root_node = build_huffman_tree(priority_queue) 
+    root_node = build_huffman_tree(priority_queue)  
+    generate_encoded_data(root_node)
     print("The root node is here", root_node)
 
 def lowest_frequency_nodes(queue): 
@@ -77,8 +80,8 @@ def lowest_frequency_nodes(queue):
 def merge(node1:PrioritizedItem, node2:PrioritizedItem): 
     total_frequency = node1.priority + node2.priority
     merged_node = Huffman_Node(total_frequency) 
-    merged_node.leftNode = Huffman_Node(node1.item) 
-    merged_node.rightNode = Huffman_Node(node2.item) 
+    merged_node.leftNode = node1.item    
+    merged_node.rightNode = node2.item
     return merged_node 
     
     
@@ -91,8 +94,26 @@ def build_huffman_tree(queue:PriorityQueue):
     
     return queue.get().item
     
-   
-
+def generate_encoded_data(root_node):  
+    # doing BFS  
+    print("Generating the encoded data...")
+    queue = []
+    queue.append(root_node)
+    while(len(queue)>=1): 
+        node:Huffman_Node = queue.pop() 
+        if(type(node.leftNode) !=str): 
+            print("The left node is", node.leftNode)
+            queue.append(node.leftNode) 
+        else:
+            print("Data on the left is", node.leftNode) 
+        if(type(node.rightNode)!=str):
+            queue.append(node.rightNode) 
+        else:
+            print("Data on the right is", node.rightNode) 
+            
+       
+    
+    pass
 def huffman_decoding():
     pass  
 
