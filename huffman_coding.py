a9cd417fe8fa5f336efe1d3c1d0982e03551b59e
Kunal Dubey <kunaldubey2297@gmail.com>
2021-12-09 21:49:16 +0530

    able to encode string

--- huffman_coding.py
+++ huffman_coding.py
@@ -67,17 +67,26 @@ def create_priority_queue(frequency_dictionary:dict):
         for letter in frequency_dictionary.keys():  
             node = Huffman_Node(frequency_dictionary[letter],'leaf',letter)
             queue.put(PrioritizedItem(frequency_dictionary[letter],node))
-        print("Lowest prirority", queue.get())
+        # print("Lowest prirority", queue.get())
         return queue
      
 def huffman_encoding(string :str): 
     frequency_dictionary = determine_frequency(string)   
+    print("Frequency Dictionary is",frequency_dictionary) 
     priority_queue = create_priority_queue(frequency_dictionary) 
-    root_node = build_huffman_tree(priority_queue)  
-    generate_encoded_data(root_node,'')
-    print("The root node is here", root_node)
+    root_node = build_huffman_tree(priority_queue)
+    generate_encoded_data(root_node,'',frequency_dictionary) 
+    #print("Modified dictionary is", frequency_dictionary) 
+    encoded_string = '' 
+    for letter in string:
+        encoded_string = encoded_string + frequency_dictionary[letter]['code']  
+    #print("Encoded string is",encoded_string) 
+    return encoded_string
+    
+    #print("The root node is here", root_node) 
 
 def lowest_frequency_nodes(queue): 
+    #print("Queue is",queue.queue)
     return queue.get(), queue.get()
     pass 
 
@@ -93,17 +102,22 @@ def build_huffman_tree(queue:PriorityQueue):
     while(queue.qsize()>1):
         node1, node2 = lowest_frequency_nodes(queue) 
         merged_node = merge(node1, node2)  
-        print("Combined frequency of the merged nodes is", merged_node.frequency)
+        #print("Combined frequency of the merged nodes is", node1.item.data, node2.item.data)
         queue.put(PrioritizedItem(merged_node.frequency,merged_node)) 
     
     return queue.get().item
     
-def generate_encoded_data(root_node:Huffman_Node, code):  
+def generate_encoded_data(root_node:Huffman_Node, code,dict):  
     # doing BFS   
-    if(root_node.node_type =='parent'):
-        generate_encoded_data(root_node.leftNode,code=code+'0')
-        generate_encoded_data(root_node.rightNode, code=code+'1')
-    print('code is', code, "Frequency is", root_node.frequency)
+    if(root_node.node_type =='parent'): 
+        left_code = code+'0'
+        right_code = code+'1'
+        generate_encoded_data(root_node.leftNode,left_code,dict)
+        generate_encoded_data(root_node.rightNode,right_code,dict) 
+    elif(root_node.node_type == 'leaf'):
+        frequency = dict[root_node.data]   
+        dict[root_node.data] = {'code':code, 'frequency':frequency}
+        return code,root_node.data
 
 def huffman_decoding():
     pass  
@@ -111,6 +125,7 @@ def huffman_decoding():
 if __name__ == "__main__":
     codes = {}
 
-    a_great_sentence = "The bird is the word" 
+    a_great_sentence = "AAAAAAABBBCCCCCCCDDEEEEEE" 
     
-    queue = huffman_encoding(a_great_sentence)
+    encoded_string = huffman_encoding(a_great_sentence) 
+    print(encoded_string)
