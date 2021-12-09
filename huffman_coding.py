11d222bf287bc02af43872f666a69dc8f3cf3e59
Kunal Dubey <kunaldubey2297@gmail.com>
2021-12-09 22:02:11 +0530

    able to decode the string

--- huffman_coding.py
+++ huffman_coding.py
@@ -74,14 +74,14 @@ def huffman_encoding(string :str):
     frequency_dictionary = determine_frequency(string)   
     print("Frequency Dictionary is",frequency_dictionary) 
     priority_queue = create_priority_queue(frequency_dictionary) 
-    root_node = build_huffman_tree(priority_queue)
+    root_node = build_huffman_tree(priority_queue) 
     generate_encoded_data(root_node,'',frequency_dictionary) 
     #print("Modified dictionary is", frequency_dictionary) 
     encoded_string = '' 
     for letter in string:
         encoded_string = encoded_string + frequency_dictionary[letter]['code']  
     #print("Encoded string is",encoded_string) 
-    return encoded_string
+    return encoded_string,root_node
     
     #print("The root node is here", root_node) 
 
@@ -119,7 +119,29 @@ def generate_encoded_data(root_node:Huffman_Node, code,dict):
         dict[root_node.data] = {'code':code, 'frequency':frequency}
         return code,root_node.data
 
-def huffman_decoding():
+def huffman_decoding(encoded_string,root_node:Huffman_Node):
+    decoded_string = ''  
+    print("Root node is",root_node)
+    temp_root:Huffman_Node = root_node
+    for bit in encoded_string:
+        if(bit=='0'): 
+            temp_root = temp_root.leftNode  
+            if(temp_root.node_type == 'leaf'):
+                print("temp root is", temp_root.node_type)
+                decoded_string = decoded_string + temp_root.data
+                temp_root = root_node 
+        elif(bit =='1'):
+             temp_root = temp_root.rightNode 
+             if(temp_root.node_type == 'leaf'):
+                decoded_string = decoded_string + temp_root.data
+                temp_root = root_node 
+    print("Decoded string is", decoded_string)
+    return decoded_string
+        
+
+
+
+
     pass  
 
 if __name__ == "__main__":
@@ -127,5 +149,7 @@ if __name__ == "__main__":
 
     a_great_sentence = "AAAAAAABBBCCCCCCCDDEEEEEE" 
     
-    encoded_string = huffman_encoding(a_great_sentence) 
-    print(encoded_string)
+    encoded_string,root_node = huffman_encoding(a_great_sentence)  
+    decoded_string = huffman_decoding(encoded_string,root_node)
+    print(encoded_string) 
+    print(decoded_string)
